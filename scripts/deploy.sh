702aa12ff7dfefbf92c5dc2298fd7ff85b07eaf1
bbortt <timon.borter@post.ch>
2018-08-18 22:11:51 +0200

    remove windows line endings.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,8 @@
-﻿#!/bin/bash
+#!/bin/bash
 # Deployment script for TBME Labs Actuator Endpoints Security Utils
 # https://github.com/tbmelabs/actuator-endpoints-security-utils
 
-if [[ $TRAVIS_BRANCH == "master" && $TRAVIS_PULL_REQUEST == "false" ]]; then
+if [[ $TRAVIS_BRANCH == "master" && $TRAVIS_PULL_REQUEST == "false" ]] ; then
   mvn clean versions:set deploy -Psign,build-extras -DremoveSnapshot -DskipTests
 fi
+
